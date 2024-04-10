61cbbe1465e23277203184a6ac8b0a2aaaa0ca35
fanghaizhou <fanghaizhou611@163.com>
2024-04-11 01:42:10 +0800

    first commit

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fanghaizhou/project/crypto_pre/common/df_alg.cc" "CMakeFiles/test.dir/common/df_alg.cc.o" "gcc" "CMakeFiles/test.dir/common/df_alg.cc.o.d"
+  "/home/fanghaizhou/project/crypto_pre/common/parquet_io.cc" "CMakeFiles/test.dir/common/parquet_io.cc.o" "gcc" "CMakeFiles/test.dir/common/parquet_io.cc.o.d"
+  "/home/fanghaizhou/project/crypto_pre/common/time_utils.cc" "CMakeFiles/test.dir/common/time_utils.cc.o" "gcc" "CMakeFiles/test.dir/common/time_utils.cc.o.d"
+  "/home/fanghaizhou/project/crypto_pre/common/utils.cc" "CMakeFiles/test.dir/common/utils.cc.o" "gcc" "CMakeFiles/test.dir/common/utils.cc.o.d"
+  "/home/fanghaizhou/project/crypto_pre/main.cc" "CMakeFiles/test.dir/main.cc.o" "gcc" "CMakeFiles/test.dir/main.cc.o.d"
+  "/home/fanghaizhou/project/crypto_pre/websocket_client.cc" "CMakeFiles/test.dir/websocket_client.cc.o" "gcc" "CMakeFiles/test.dir/websocket_client.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
